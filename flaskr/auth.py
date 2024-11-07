0cc68db47b950f915dd90eb3417b7feebe3405d8
s-blomberg <s_blomberg@mail.fhsu.edu>
2024-11-06 20:20:12 -0600

    Minor cleanup, modified schema for albums table

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -3,17 +3,12 @@
 # Mini Project 3
 
 import functools
-
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from flaskr.db import get_db
 
 #/auth
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-
 
 
 #/auth/register
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,11 +2,8 @@
 # Stephen Blomberg
 # Mini Project 3
 
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import (Blueprint, flash, g, redirect, render_template, request, url_for)
 from werkzeug.exceptions import abort
-
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
